dc3b601d0b00aa4477d896700005b2453e543f9e
Urquiza <urquiza-nobrega@outlook.com>
2020-08-15 17:11:43 -0300

    refactoring: created routes and controller

---
+++ src/controllers/UserController.js
@@ -0,0 +1,8 @@
+const knex = require('../database')
+
+module.exports = {
+    async getUsers(req, res) {
+        const results = await knex('users')
+        return res.json(results)
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const routes = require('./routes')
 
 const server = express()
+
+server.use(routes)
 
 server.listen(3333, () => console.log('Server is running'))
